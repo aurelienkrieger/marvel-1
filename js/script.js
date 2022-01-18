1fa1a534332165953cf40704e9c3696c611fe36b
Aurélien Krieger <a.krieger.b@gmail.com>
2022-01-18 19:21:52 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$("#hide").click(function(){
+  $("img").hide();
+  $(document.body).css("background-color", "#ffffff");
+});
+
+$("#show").click(function(){
+  $("img").show();
+  $(document.body).css("background-color", "#06c91d");
+});
